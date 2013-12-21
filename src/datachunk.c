d239652594eac1e4c9b7337f4b7521eda7effa5b
ZachAnders <Zacharia.Anders@gmail.com>
2013-12-20 23:18:42 -0700

    Hashes need to iterate over all chunks in a datachunk.

--- src/datachunk.c
+++ src/datachunk.c
@@ -1,8 +1,13 @@
 #include "datachunk.h"
 #include "openssl/sha.h"
 
+void calculate_hash(struct datachunk *chunk) {
+	uint32_t curs;
+	SHA512_Init(&chunk->ctx);
+	for (curs = 0; curs < chunk->num_chunks; curs++) {
+		SHA512_Update(&chunk->ctx, chunk->data[curs], chunk->data_len[curs]);
+	}
+	SHA512_Final(chunk->hash, &chunk->ctx);
 
-void calculate_hash(struct datachunk *a_chunk) {
-	SHA512((unsigned char *) a_chunk->data, a_chunk->data_len, (unsigned char *) a_chunk->hash);
 }
 
--- src/datachunk.h
+++ src/datachunk.h
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <openssl/sha.h>
 
 #ifndef _DATACHUNK_H_
 #define _DATACHUNK_H_
 
 #define MAX_DIGEST_SIZE 64
+#define MAX_CHUNK_SIZE 64
 
+/* Holds an array of strings. These strings are concatenated during hasing */
 struct datachunk {
-	short data_len;
-	const char *data;
-	const char hash[MAX_DIGEST_SIZE];
+	SHA512_CTX ctx;
+	uint32_t num_chunks;
+	const uint8_t *data[MAX_CHUNK_SIZE];
+	uint32_t data_len[MAX_CHUNK_SIZE];;
+	uint8_t hash[MAX_DIGEST_SIZE];
 };
 
 void calculate_hash(struct datachunk *a_chunk);
